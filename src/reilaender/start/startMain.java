35efe0f454669b05fe4cbd44a27fcaba31d3aed9
mreilaender <mreilaender@student.tgm.ac.at>
2015-01-21 10:47:02 +0100

    Removed unimportand code alternatively the comments

--- src/reilaender/start/startMain.java
+++ src/reilaender/start/startMain.java
@@ -28,30 +28,5 @@ public class startMain {
 			System.err.println(todo);
 			e.printStackTrace();
 		}
-//		try {
-//			meta = connector.getCon().getMetaData();
-//			ResultSet tmp = connector.executeQuery("SHOW TABLES");
-//			System.out.println("\n\n" + tmp.getString(1) + "\n\n\n");
-//			while(tmp.next()) {
-//				System.out.println("-- Table " + tmp.getString(1) + " --");
-//				ResultSet metat = meta.getPrimaryKeys(null, null, tmp.getString(1));
-//				
-//				metat.next();
-//				for(int j = 1;j < 10;++j) {
-//					try {
-//						System.out.print(metat.getString(j) + " ; ");
-//					} catch(SQLException e) {
-//						System.err.print("Out of Range ; ");
-//					} catch(NullPointerException e) {
-//						System.err.print("Null " + " ; ");
-//					}
-//				}
-//				System.out.println();
-//			}
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			//			System.err.println(todo);
-//			e.printStackTrace();
-//		}
 	}
 }
